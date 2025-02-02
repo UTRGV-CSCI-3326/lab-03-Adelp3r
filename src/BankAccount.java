df9e8676a7ad644bceb8cff68cfa955092efa823
Adelp3r <adel.perales01@utrgv.edu>
2025-02-02 05:05:04 +0000

    2/1/2025 11:05 PM

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,15 @@
+public class BankAccount {
 
+    public static void main(String[] args){
+        double bobBank = 2175.37;
+        bobBank -= 302.50;
+        bobBank += 50.03;
+        bobBank = bobBank/2.0 + 20.00;
+        bobBank--;
+        bobBank *= 2;
+        bobBank++;
+
+        System.out.println("Bob's new balance: " + bobBank);
+    }
+
+}
